ee28ac2f9c63a3377658b24472cd2549c810dc52
Lahari <Lahari@DESKTOP-TCUAEQ3>
2019-10-20 23:56:03 +0530

    commit1

---
+++ LaunchingChrome/src/test/java/LaunchChromeTest.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchChromeTest {
+
+@Test
+public void launchChrome()
+{
+	WebDriver driver=new ChromeDriver();
+}
+}
